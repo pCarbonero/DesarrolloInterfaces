e9de4a10e2e60f3e1b6ca2568e05517f5c1ba783
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-04 14:39:23 +0200

    [new] ej maui

---
+++ Tema5/Ejercicio01Tema5/ClasesMauiHM/ClsPersona.cs
@@ -0,0 +1,16 @@
+﻿namespace ClasesMauiHM
+{
+    public class ClsPersona
+    {
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+
+        public ClsPersona(string nombre, string apellidos)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+        }
+
+        public ClsPersona() { }
+    }
+}
---
+++ Tema5/Ejercicio01Tema5/MauiHolaMundo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ClasesMauiHM;
+
+namespace MauiHolaMundo
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+           ClsPersona pers = new ClsPersona();
+           pers.Nombre = entry.Text;
+          // string result = await DisplayPromptAsync("Apellidos", "¿Cual es su apellido?");
+
+            /* count++;
+
+             if (count == 1)
+                 CounterBtn.Text = $"Clicked {count} time";
+             else
+                 CounterBtn.Text = $"Clicked {count} times";
+
+             SemanticScreenReader.Announce(CounterBtn.Text);*/
+        }
+    }
+
+}
