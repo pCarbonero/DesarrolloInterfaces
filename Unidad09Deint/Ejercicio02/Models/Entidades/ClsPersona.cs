65e1d64293050d4009c785f412ee699a7fadfcb6
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-04 14:32:24 +0100

    [new] ejercicio 02

---
+++ Unidad09Deint/Ejercicio02/Models/Entidades/ClsPersona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02.Models.Entidades
+{
+    internal class ClsPersona: INotifyPropertyChanged
+    {
+        #region atributos
+        private string nombre;
+        #endregion
+
+        #region propiedades
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; notifyPropertyChanged("Nombre"); }
+        }
+        #endregion
+
+
+        #region notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void notifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
