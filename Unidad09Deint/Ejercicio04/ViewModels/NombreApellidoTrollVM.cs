a1091689c55abc89c3f895556532750477930eba
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-11 14:37:08 +0100

    [new] ejercicio04

---
+++ Unidad09Deint/Ejercicio04/ViewModels/NombreApellidoTrollVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04.ViewModels
+{
+    public class NombreApellidoTrollVM: INotifyPropertyChanged
+    {
+        #region atributos
+        private string nombre;
+        private string apellido;
+        #endregion
+
+        #region Propiedades
+        public string Nombre 
+        {
+            get { return nombre; }
+            set 
+            {
+                nombre = value;
+                
+                if (nombre.Contains("n"))
+                {
+                    apellido = "";
+                }
+                notifyPropertyChanged("Apellido");
+            }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set 
+            {
+                apellido = value;
+                
+                if (apellido.Contains("n"))
+                {
+                    nombre = "";
+                }
+                notifyPropertyChanged("Nombre");
+            }
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void notifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
