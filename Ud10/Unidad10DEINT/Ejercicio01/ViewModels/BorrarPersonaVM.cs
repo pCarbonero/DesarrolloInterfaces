55ef9c66d0f54cc55f3dfb90e5e10dad4408bab8
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-14 12:57:13 +0100

    [upd] ej01 ud10

---
+++ Ud10/Unidad10DEINT/Ejercicio01/ViewModels/BorrarPersonaVM.cs
@@ -0,0 +1,83 @@
+﻿using DAL;
+using Entidades;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ejercicio01.ViewModels
+{
+    internal class BorrarPersonaVM: INotifyPropertyChanged
+    {
+        #region atributos
+        private clsPersona personaSeleccionada;
+        private DelegateCommand eliminar;
+        #endregion
+
+        #region propiedades
+        public clsPersona PersonaSeleccionada
+        {
+            get { return personaSeleccionada; }
+
+            set 
+            { 
+                personaSeleccionada = value;
+                notifyPropertyChanged("PersonaSeleccionada");
+                eliminar.RaiseCanExecuteChanged();
+            }
+        }
+        public DelegateCommand Eliminar
+        {
+            get { return eliminar; }
+        }
+        public ObservableCollection<clsPersona> ListaPersonas { get; }
+        #endregion
+
+        #region constructores
+        public BorrarPersonaVM()
+        {
+            try
+            {
+                ListaPersonas = new ObservableCollection<clsPersona>(clsListados.listadoPersonas());
+                eliminar = new DelegateCommand(Execute, CanExecute);
+            }
+            catch (Exception ex)
+            {
+                // TODO: mostrar mensaje de ERROU $%&LAVICBIDNIINMLAVICBIDNIINM$%&
+            }
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void notifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Eventos
+        public event EventHandler? CanExecuteChanged;
+        private bool CanExecute()
+        {
+            bool sePuede = true;
+            if (personaSeleccionada == null)
+            {
+                sePuede = false;
+            }
+            return sePuede;
+        }
+
+        public void Execute()
+        {
+           ListaPersonas.Remove(PersonaSeleccionada);
+        }
+        #endregion
+    }
+}
---
+++ Ud10/Unidad10DEINT/Entidades/clsPersona.cs
@@ -0,0 +1,27 @@
+﻿namespace Entidades
+{
+    public class clsPersona
+    {
+        #region Propiedades
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime fechaNac { get; set; }
+        public string Foto { get; set; }
+        public string Direccion { get; set; }
+        public int Tlfn { get; set; }
+        #endregion
+
+        #region constructores
+        public clsPersona() { }
+        public clsPersona(string nombre, string apellidos, DateTime fechaNac, string foto, string direccion, int tlfn)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            this.fechaNac = fechaNac;
+            Foto = foto;
+            Direccion = direccion;
+            Tlfn = tlfn;
+        }
+        #endregion
+    }
+}
