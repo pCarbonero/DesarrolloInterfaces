2816a2e3c56390666c9803fca5d9015f2a8070a3
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-28 14:40:52 +0100

    [upd] que

--- Practicas/Examen1/Examen(asp)Maui/BL/clsListadosBL.cs
+++ Practicas/Examen1/Examen(asp)Maui/BL/clsListadosBL.cs
@@ -22,6 +22,38 @@ namespace BL
         }
 
         /// <summary>
+        /// Funcion estatica que se encarga de filtrar la lista
+        /// </summary>
+        /// <pre>la dificultad debe ser de 1 a 5</pre>
+        /// <pos>si no hay candidatos disponibles se enviará una lista vacia</pos>
+        /// <param name="dificultad"></param>
+        /// <returns></returns>
+        public static List<clsCandidato> listadoCandidatosParaMision(int dificultad)
+        {
+            List<clsCandidato> listaFiltrada = new List<clsCandidato>();
+            List<clsCandidato> listaCompleta = clsListadosBL.listadoCompletoCandidatosBL();
+
+            switch (dificultad)
+            {
+                case 1 or 2: 
+                    listaFiltrada = listaCompleta.Where(per => per.Nacionalidad == "Usa").ToList();
+                    break;
+                case 3:
+                    listaFiltrada = listaCompleta.Where(per => per.Nacionalidad == "Usa" && 
+                    (DateTime.Now.Year - per.FechaNac.Year - (DateTime.Now.DayOfYear < per.FechaNac.DayOfYear ? 1 : 0)) >= 40).ToList();
+                    break;
+                case 4:
+                    listaFiltrada = listaCompleta.Where(per => per.Nacionalidad == "Italia" && DateTime.Now.Year - per.FechaNac.Year < 45).ToList();
+                    break;
+                case 5:
+                    listaFiltrada = listaCompleta.Where(per => per.Nacionalidad == "Italia" && 
+                    (DateTime.Now.Year - per.FechaNac.Year >= 45 && DateTime.Now.Year - per.FechaNac.Year <= 55)).ToList();
+                    break;
+            }
+            return listaFiltrada;
+        }
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="id"></param>
--- Practicas/Examen1/Examen(asp)Maui/Examen(asp)Maui/ViewModels/clsSeleccionarMisionVM.cs
+++ Practicas/Examen1/Examen(asp)Maui/Examen(asp)Maui/ViewModels/clsSeleccionarMisionVM.cs
@@ -1,34 +1,51 @@
-﻿using BL;
+﻿using _18_CRUD_Personas_UWP_UI.ViewModels.Utilidades;
+using BL;
 using Entidades;
 using Examen_asp_Maui.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Examen_asp_Maui.ViewModels
 {
-    public class clsSeleccionarMisionVM
+    public class clsSeleccionarMisionVM: INotifyPropertyChanged
     {
         #region atributos
-        private List<clsCandidatoEdad> listaCandidatosEdad;
+        private ObservableCollection<clsCandidatoEdad> listaCandidatosEdad;
         private List<clsCandidato> listaCandidatos;
         private List<clsMision> listadoMisiones;
 
-        clsMision misionSeleccionada;
-        clsCandidatoEdad candidatoSeleccionado;
+        private clsMision misionSeleccionada;
+        private clsCandidatoEdad candidatoSeleccionado;
+
+        private DelegateCommand detallesCommand;
 
         #endregion
 
         #region Propiedades
-        public List<clsCandidatoEdad> ListaCandidatosEdad { get { return listaCandidatosEdad; } }
+        public ObservableCollection<clsCandidatoEdad> ListaCandidatosEdad 
+        {
+            get { return listaCandidatosEdad; } 
+            set 
+            {
+                listaCandidatosEdad = value;
+            }
+        }
         public List<clsMision> ListadoMisiones { get { return listadoMisiones; } }
 
         public clsMision MisionSeleccionada
         {
             get { return misionSeleccionada; }
-            set { misionSeleccionada = value; }
+            set 
+            { 
+                misionSeleccionada = value;
+                rellenarListaCandidatos();
+            }
         }
 
         public clsCandidatoEdad CandidatoSeleccionado
@@ -37,12 +54,74 @@ namespace Examen_asp_Maui.ViewModels
             set { value = candidatoSeleccionado; }
         }
 
+        public DelegateCommand DetallesCommand
+        {
+            get { return detallesCommand; }
+        }
         #endregion
 
+        #region constructores
         public clsSeleccionarMisionVM()
         {
-            listaCandidatosEdad = new List<clsCandidatoEdad>();
+            listaCandidatosEdad = new ObservableCollection<clsCandidatoEdad>();
             listadoMisiones = clsListadosBL.listadoCompletoMisionesBL();
+            detallesCommand = new DelegateCommand(detallesExecute, detallesCanExecute);
         }
+        #endregion
+
+        #region Command
+        /// <summary>
+        /// Funcion que se encarga de ejecutar las instrucciones del comando
+        /// </summary>
+        public async void detallesExecute()
+        {
+
+        }
+
+        /// <summary>
+        /// Metodo que comprueba si el comando verCandidatos se puede ejecutar
+        /// </summary>
+        /// <returns></returns>
+        public bool detallesCanExecute()
+        {
+            bool canExecute = false;
+            if (misionSeleccionada != null)
+            {
+                canExecute = true;
+            }
+            return canExecute;
+        }
+
+        #endregion
+
+        #region metodos
+        private async void rellenarListaCandidatos()
+        {
+            try
+            {
+                listaCandidatos = clsListadosBL.listadoCandidatosParaMision(misionSeleccionada.Dificultad);
+                listaCandidatosEdad.Clear();
+                foreach (clsCandidato candidato in listaCandidatos)
+                {
+                    listaCandidatosEdad.Add(new clsCandidatoEdad(candidato));
+                }
+                NotifyPropertyChanged("ListaCandidatosEdad");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", "You have been alerted", "OK");
+            }
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new
+            PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
