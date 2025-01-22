f5abaf4ba0d52bcc0e649bac650a89b982ac3bae
pCarbonero <pablo.carbonero@iesnervion.es>
2025-01-22 14:40:08 +0100

    [new] maui mas o menos

---
+++ Unidad12/Chat/ChatSignalR/ChatMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Unidad12/Chat/ChatSignalR/ChatMaui/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Models;
+using System.Collections.ObjectModel;
+
+namespace ChatMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public readonly HubConnection connection;
+        public ObservableCollection<clsMensajeUsuario> mensajes = new ObservableCollection<clsMensajeUsuario>();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            connection = new HubConnectionBuilder().WithUrl("http://localhost:5250/chathub").Build();
+
+            connection.On<clsMensajeUsuario>("ReceiveMessage", (message) =>
+            {
+                mensajes.Add(message);
+            });
+
+            Task.Run(() =>
+            {
+                Dispatcher.Dispatch(async () =>
+                await connection.StartAsync());
+            });
+        }
+        public async void OnSendClicked(object sender, EventArgs e)
+        {
+            clsMensajeUsuario mensaje = new clsMensajeUsuario();
+            mensaje.NombreUsuario = username.Text;
+            mensaje.MensajeUsuario = message.Text;
+            await connection.InvokeCoreAsync("SendMessage", args: new[] { mensaje });
+           // mensajes.Add(mensaje);
+
+            username.Text = String.Empty;
+            message.Text = String.Empty;
+        }
+    }
+
+}
